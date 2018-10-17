e7ba2e7338dad5020829a87902e969cb60f88184
Kostas Evangelou <kostas.ev79@gmail.com>
2018-10-17 23:55:52 +0300

    Added options and read data file

--- constants.h
+++ constants.h
@@ -4,8 +4,12 @@
 
 #ifndef UNTITLED_CONSTANTS_H
 #define UNTITLED_CONSTANTS_H
+
+#include <iostream>
+
 namespace const_lsh {
     const int k = 4;
     const int L = 5;
+    const std::string def_func = "euclidean";
 }
 #endif //UNTITLED_CONSTANTS_H
--- main.cpp
+++ main.cpp
@@ -1,11 +1,27 @@
 #include <iostream>
+#include <string>
+#include <fstream>
 #include <getopt.h>
 #include "./constants.h"
 
 using namespace std;
 
-int main(int argc,char** argv) {
-    string inputfd, queryfd, outputfd="";
+int num_columns(string line) {
+    char prev=' ';
+    int words=0;
+    for(int i=0;i<line.size(); i++){
+        if ((isspace(line[i]))&&(!isspace(prev))){
+            words++;
+        } else {
+            prev = line[i];
+        }
+    }
+    return words+1;
+}
+
+int main(int argc, char** argv) {
+    string input="", query="", output="";
+    string func_name = "";
     bool read_query;
     int c;
     int k=-1;
@@ -13,14 +29,13 @@ int main(int argc,char** argv) {
 
     /* Reading Arguments from command line */
 
-    while(( c = getopt(argc,argv,"d:q:k:L:o:")) !=-1 ){
-
+    while(( c = getopt(argc,argv,"d:q:k:L:o:f:h")) !=-1 ){
         switch (c) {
             case 'd':
-                inputfd = optarg;
+                input = optarg;
                 break;
             case 'q':
-                queryfd = optarg;
+                query = optarg;
                 break;
             case 'k':
                 k = atoi(optarg);
@@ -29,8 +44,21 @@ int main(int argc,char** argv) {
                 L = atoi(optarg);
                 break;
             case 'o':
-                outputfd = optarg;
+                output = optarg;
                 break;
+            case 'f':
+                func_name = optarg;
+                break;
+            case 'h':
+                cout << "\nlsh - Locality Sensitive Hashing\n" << endl <<
+                     "Mandatory options\n\t-d data feed file\n\n"
+                     "Optional flags\n"
+                     "\t-q query data file\n"
+                     "\t-k num of random probes\n"
+                     "\t-L num of hash tables\n"
+                     "\t-o output file\n"
+                     "\t-f function to use (euclidean and cosine similarity available)" << endl;
+                return 0;
             case '?':
                 cerr << "Unknown option character " << char(optopt) << endl;
                 return 1;
@@ -38,19 +66,41 @@ int main(int argc,char** argv) {
                 abort();
         }
     }
-    if (inputfd.size()==0) {
+    if (input.size()==0) {
         cerr << "Data file was not defined!" << endl ;
     }
     if (k==-1) k=const_lsh::k;
     if (L==-1) L=const_lsh::L;
-    if (queryfd.size()==0) read_query=true;
-    if (outputfd.size()==0) outputfd = "./output.txt";
+    if (query.size()==0) read_query=true;
+    if (output.size()==0) output = "./output.txt";
+    if (func_name.size()==0) func_name = const_lsh::def_func;
 
     cout << "L: " << L << endl;
     cout << "k: " << k << endl;
-    cout << "in: " << inputfd  << endl;
-    cout << "qr: " << queryfd  << endl;
-    cout << "ou: " << outputfd << endl;
+    cout << "in: " << input  << endl;
+    cout << "qr: " << query  << endl;
+    cout << "ou: " << output << endl;
+    cout << "fn: " << func_name << endl;
+
+    ifstream inputfd;
+    string line;
+    int num_lines = 0;
+    int d;
+    inputfd.open (input);
+    if (inputfd.is_open()){
+        getline(inputfd,line);
+        d = num_columns(line);
+        num_lines++;
+        while ( getline(inputfd,line) ){
+            num_lines++;
+        }
+        inputfd.close();
+    }else{
+        cerr << "Unable to open file: " << input << endl;
+    }
+
+    cout << "d: " << d << endl;
+    cout << "num lines: " << num_lines << endl;
 
     return 0;
 }
